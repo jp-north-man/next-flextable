8352315a6ab143db4604b6824f5fc26ebc13e9a6
jp-north-man <yojiro.north.man@gmail.com>
2023-03-13 22:24:58 +0900

    change header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,13 @@
 import { FaHamburger } from 'react-icons/fa';
+import Link from 'next/link';
 
 export function header(){
     return (
 
-        <header className="bg-gray-800 text-white">
+        <header className="bg-slate-50 text-black">
             <div className="flex items-center justify-between px-4 py-3">
-                <a href="index.tsx" className="font-bold text-lg">app name</a>
-                <button className="block lg:hidden focus:outline-none" id="menu-btn">
-                <FaHamburger className="w-6 h-6 fill-current" />
-                </button>
-                <nav className="hidden lg:block">
-                <ul className="inline-flex space-x-4">
-                    <li><a href="#">Menu 1</a></li>
-                    <li><a href="#">Menu 2</a></li>
-                    <li><a href="#">Menu 3</a></li>
-                </ul>
-                </nav>
-            </div>
-            <div className="hidden lg:hidden" id="mobile-menu">
-                <ul className="block px-2 py-3">
-                <li><a href="#" className="block text-white font-bold py-2">Menu 1</a></li>
-                <li><a href="#" className="block text-gray-500 hover:text-white py-2">Menu 2</a></li>
-                <li><a href="#" className="block text-gray-500 hover:text-white py-2">Menu 3</a></li>
-                </ul>
+                <a href="index.tsx" className="font-bold text-lg">FlexTable</a>
+                <Link href={'/'} className="mr-10">create</Link>
             </div>
         </header>
 
