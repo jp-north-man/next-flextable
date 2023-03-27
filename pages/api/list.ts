3a349b5307839a7cc7747cbf7cd1bd67c0bc2186
jp-north-man <yojiro.north.man@gmail.com>
2023-03-27 23:08:18 +0900

    list api

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
+    
     try {
       const response = await fetch('http://localhost:8080/list', {
         method: 'GET',
