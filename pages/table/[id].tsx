7abc3a384ed6ea078017283dba6194edd44086ad
jp-north-man <yojiro.north.man@gmail.com>
2023-04-06 21:58:20 +0900

    style change

--- pages/table/[id].tsx
+++ pages/table/[id].tsx
@@ -73,28 +73,39 @@ const Table: NextPage = () => {
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           <tr>
-            {columns.map((column, index) => (
-              <td key={index} className="px-2 py-3">
-                <div
-                  className="text-sm text-gray-900"
-                  style={{
-                    minWidth: `${column.width}px`,
-                    maxWidth: `${column.width}px`,
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    whiteSpace: 'nowrap'
-                  }}
-                >
-                  {column.type === 'text' && 'Sample text'}
-                  {column.type === 'button' && (
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
-                      Button
-                    </button>
-                  )}
-                  {column.type === 'date' && '2023-03-28'}
-                </div>
-              </td>
-            ))}
+          {columns.map((column, index) => (
+            <td key={index} className="px-2 py-3">
+              <div
+                className="text-sm text-gray-900"
+                style={{
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap'
+                }}
+              >
+                {column.type === 'text' && (
+                  <input
+                    type="text"
+                    className="border rounded p-1 sm:text-sm border-none outline-none"
+                    placeholder="Sample text"
+                    style={{
+                      width: '100%',
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      whiteSpace: 'nowrap'
+                    }}
+                  />
+                )}
+                {column.type === 'button' && (
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
+                    Button
+                  </button>
+                )}
+                {column.type === 'date' && '2023-03-28'}
+              </div>
+            </td>
+          ))}
+
           </tr>
         </tbody>
       </table>
